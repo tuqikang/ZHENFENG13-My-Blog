afff03c5987a6c8e3364ed201c14ec4aebeb8e4f
tqk <1282995880@qq.com>
2018-12-03 22:00:59 +0800

    1

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#编译+部署order站点
+
+#需要配置如下参数
+# 项目路径, 在Execute Shell中配置项目路径, pwd 就可以获得该项目路径
+# export PROJ_PATH=这个jenkins任务在部署机器上的路径
+
+# 输入你的环境上tomcat的全路径
+# export TOMCAT_APP_PATH=tomcat在部署机器上的路径
+
+cd $PROJ_PATH/myblog
+mvn clean install
+
+
+
